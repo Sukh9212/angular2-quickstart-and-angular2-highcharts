5021ff2b7f615a5b566b4bab0faf70991f5a53db
gevgeny <e.gluhotorenko@gmail.com>
2016-10-30 17:25:20 +0300

    integrate with angular-highcharts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ChartModule } from 'angular2-highcharts';
+
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [ BrowserModule, ChartModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
